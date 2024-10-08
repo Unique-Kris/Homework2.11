2986e8b364d99767944b98248e4805d8f4b9738c
Unique-Kris <titina91@mail.ru>
2024-10-08 14:57:04 +0300

    HW2.11

---
+++ src/main/java/pro/sky/skyproservice/SkyproServiceApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyproservice/SkyproServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
